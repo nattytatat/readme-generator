ca434a7b4bfbc232aa16f52105e6fa1f310a162c
Natalie <natalie@bigorangemedia.co.uk>
2023-02-17 19:13:00 +0000

    list questions

--- index.js
+++ index.js
@@ -4,9 +4,57 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'Enter your Github username',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the title of your project',
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: 'Write a short description of your project',
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Explain how users should install this project',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Explain how users can use your project',
+        },
+        {
+            type: 'checkbox',
+            name: 'licence',
+            message: 'Choose your licence type',
+            choices: ['MIT', 'Apache', 'Creative Commons', 'GNU'],
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'Please add contributors to the project',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please add test instructions',
+        },
 
-];
+    ])
+};
 
 // function to write README file
 function writeToFile(fileName, data) {
