a53a5b979ce7d476c1c65d0c6ceb9ba2412b670c
Natalie <natalie@bigorangemedia.co.uk>
2023-02-17 19:30:50 +0000

    add questions to init function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const path = require('path');
+// const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -56,12 +56,17 @@ const questions = () => {
     ])
 };
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// // function to write README file
+// function writeToFile() {
+// }
 
 // function to initialize program
 function init() {
+    questions().then((data) => {
+        const createMarkdown = generateMarkdown(data);
+        fs.writeFile('README.md', data, (err) =>
+        err ? console.log(err) : console.log('README hs been successfully generated!'));
+    })
 
 }
 
